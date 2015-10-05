fcda0f504bb1036205009165703673c94a40f2c5
Gerrit VanderLugt <gvanderlugt@joinhaven.com>
2015-10-05 12:09:59 -0400

    Can now add comments to articles.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
-  
+
   def create
-    @article = Article.find(params[:article_id])
+    @user = User.find(params[:user_id])
+    @article = @user.articles.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    redirect_to user_article_path(@user, @article)
   end
 
   private
